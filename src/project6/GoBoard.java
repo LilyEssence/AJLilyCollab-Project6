bf940984550bf83aea8ea53ed286a9c2dc285d52
arjunteh <arjun_teh@utexas.edu>
2015-11-19 14:34:26 -0600

    added Chain class

--- src/project6/GoBoard.java
+++ src/project6/GoBoard.java
@@ -1,5 +1,13 @@
 package project6;
 
 public class GoBoard {
+    public int[][] boardArray;
 
+    public GoBoard(){
+	boardArray = new int[9][9];
+    }
+
+    public GoBoard(int boardSize){
+	boardArray = new int[boardSize][boardSize];
+    }
 }
